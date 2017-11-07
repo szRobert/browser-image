6e739df8a0fd6fc73c650c99510b6866ad4ac639
robert.szabo <robert.szabo95@outlook.com>
2017-11-07 07:55:32 +0100

    css reseted, css mobile style started, burger image added..

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+/* CSS-s */
+import './css/reset.css';
+import './css/base.css';
+import './css/mob.css';
+/* .... */
+import './css/hig.css';
+/* icons */
+import burger from './img/burger.png';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className='toolbar-nav'> 
+          <h1 className='title'>BrowserImage</h1>
+          <a onClick='' className=''><img id='toolbar-menu--burger' src={burger} /></a>
+          <ul className='toolbar-menu'>
+            <li><button className='toolbar-menu--button' onClick=''>Open</button></li>
+            <li><button className='toolbar-menu--button' onClick=''>Save</button></li>
+          </ul>
+        </div>
+
+        <div className='functions-nav'>
+          <ul className='functions-menu'>
+            <li><button className='functions-menu--button' onClick=''>Rotate</button></li>
+            <li><button className='functions-menu--button' onClick=''>Mirroring</button></li>
+            <li><button className='functions-menu--button' onClick=''>Effects</button></li>
+          </ul>
+        </div>
+
+        <div className='workspace-box'>
+          <canvas id='mycanvas'> Ez itt egy canvas! </canvas>
+        </div>
+
       </div>
     );
   }
