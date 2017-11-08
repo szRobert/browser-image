1179c4ead298f3507681862128dd0e20c8c856a2
robert.szabo <robert.szabo95@outlook.com>
2017-11-08 06:44:37 +0100

    css update

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
 
         <div className='toolbar-nav'> 
           <h1 className='title'>BrowserImage</h1>
-          <a onClick='' className=''><img id='toolbar-menu--burger' src={burger} /></a>
-          <ul className='toolbar-menu hidden'>
+          <a onClick='' className='toolbar-nav--burger-button'><img id='toolbar-nav--burger' src={burger} /></a>
+          <ul className='toolbar-menu visible'>
             <li><a className='toolbar-menu--button' onClick=''>Open</a></li>
             <li><a className='toolbar-menu--button' onClick=''>Save</a></li>
           </ul>
